96ec82f989d2ead4fa18b05845a126dc3b808efc
Bruno <pazosbruno@gmail.com>
2019-07-12 17:52:01 -0300

    update server routes + added js ajax call to interact with the server and update the frontend

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home(name=None):
     return render_template('game.html', name=name)
 
-@app.route('/procesar/<palabra>')
+@app.route('/procesar/<palabra>', methods = ['POST'])   
 def procesar_palabra(palabra=None):
-    pass
+    
+    data = {
+        'word': palabra, #la palabra original
+        'answer': "Nocivo", #La palabra respuesta o cadena vacía si no tiene respuesta
+        'state': 0, #0: seguimos, 1: Gana server, 2: Gana participante
+        'score': 0 #cantidad de rondas que el participante pudo continuar jugando.
+    }
+
+    return jsonify(data)
