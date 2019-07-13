9be2775a9e0b0ea01356832688dab87415380bb0
Pablo <pablo1n7@gmail.com>
2019-07-13 20:40:17 -0300

    server init

--- server.py
+++ server.py
@@ -4,19 +4,15 @@ from Game import Game, ERROR_INVALID_RULE, ERROR_WORD_USED, ERROR_UNSYLLABLE, OK
 
 app = Flask(__name__)
 
-def init_g():
-    return Game("test_palabras_espanol.txt")
+g = Game("test_palabras_espanol.txt")
 
 @app.route('/')
 def home(name=None):
-    g = init_g()
     _, word, _ = g.run()
     return render_template('game.html', name=name, word=word)
 
 @app.route('/process/<word>', methods = ['POST'])   
 def process_word(word=None):
-
-    g = init_g()
     gamer, w, error = g.run(word)
     print(g.state.bw.last_word, w)
     
